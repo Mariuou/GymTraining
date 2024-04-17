c392c4cc2468c73f968868a73eec001bc6f03bed
suri <suripeli04@gmail.com>
2024-04-17 01:29:15 -0600

    cambios de diseños de dashboard

--- FrontEnd/src/views/dashboards/Training/Dashboard/dashboard.vue
+++ FrontEnd/src/views/dashboards/Training/Dashboard/dashboard.vue
@@ -1,70 +1,96 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue';
+/* Breadcrumb component */
+import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
 
-import BaseBreadcrumb from "@/components/shared/BaseBreadcrumb.vue";
-import WelcomeCard from "@/components/dashboards/analytical/WelcomeCard.vue";
-import TheEarnings from "@/components/dashboards/analytical/TheEarnings.vue";
-import MonthlyRevenue from "@/components/dashboards/analytical/MonthlyRevenue.vue";
-import TheSalesOverview from "@/components/dashboards/analytical/TheSalesOverview.vue";
-import TotalSales from "@/components/dashboards/analytical/TotalSales.vue";
-import ProductMonthTable from "@/components/dashboards/analytical/ProductMonthTable.vue";
-import BlogCard from "@/components/dashboards/analytical/BlogCard.vue";
-import WeeklyStats from "@/components/dashboards/analytical/WeeklyStats.vue";
-import DailyActivities from "@/components/dashboards/analytical/DailyActivities.vue";
+/*Call Components*/
+import Followers from '@/components/widgets/charts/Followers.vue';
+import Views from '@/components/widgets/charts/Views.vue';
+import Earned from '@/components/widgets/charts/Earned.vue';
+import TotalEarning from '@/components/widgets/charts/TotalEarning.vue';
+import CurrentValue from '@/components/widgets/charts/CurrentValue.vue';
+import MostVisited from '@/components/widgets/charts/MostVisited.vue';
+import PageImpression from '@/components/widgets/charts/PageImpression.vue';
+import YearlyBreakup from '@/components/widgets/charts/YearlyBreakup.vue';
+import MonthlyEarnings from '@/components/widgets/charts/MonthlyEarnings.vue';
+import YearlySales from '@/components/widgets/charts/YearlySales.vue';
+import RevenueUpdates from '@/components/widgets/charts/RevenueUpdates.vue';
+import SalesOverview from '@/components/widgets/charts/SalesOverview.vue';
+import Customers from "@/components/widgets/charts/Customers.vue";
+import Projects from "@/components/widgets/charts/Projects.vue";
 
-const page = ref({ title: "Dashboard de seguimiento" });
+// theme breadcrumb
+const page = ref({ title: 'Charts' });
 const breadcrumbs = ref([
-  {
-    text: "Dashboard",
-    disabled: false,
-    href: "#",
-  },
-  {
-    text: "Analytical",
-    disabled: true,
-    href: "#",
-  },
+    {
+        text: 'Home',
+        disabled: false,
+        href: '#'
+    },
+    {
+        text: 'Charts',
+        disabled: true,
+        href: '#'
+    }
 ]);
 </script>
-
 <template>
-  <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
-  <v-row>
-    <!---Welcome cards / Earnings / monthlyrevenue-->
-    <v-col cols="12" sm="12" lg="6">
-      <!-- <WelcomeCard /> -->
-      <v-row class="mt-4">
-        <v-col cols="12" sm="6" lg="6" class="">
-          <!-- <TheEarnings/> -->
+    <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>        
+    <v-row>
+        <!-- Followers Chart -->
+        <v-col cols="12" sm="3">
+            <Followers />
         </v-col>
-        <v-col cols="12" sm="6" lg="6" class="">
-          <!-- <MonthlyRevenue/> -->
+        <!-- Views Chart -->
+        <v-col cols="12" sm="3">
+            <Views />
         </v-col>
-      </v-row>
-    </v-col>
-    <!---Sales overview-->
-    <v-col cols="12" sm="12" lg="6">
-      <TheSalesOverview/>
-    </v-col>
-     <!---Total sales-->
-    <v-col cols="12" sm="12" lg="4">
-      <!-- <TotalSales/> -->
-    </v-col>
-     <!---Product month table-->
-    <v-col cols="12" sm="12" lg="8">
-      <!-- <ProductMonthTable/> -->
-    </v-col>
-     <!---Blog card-->
-    <v-col cols="12" sm="12" lg="4">
-      <!-- <BlogCard/> -->
-    </v-col>
-    <!---Weekly states-->
-    <v-col cols="12" sm="12" lg="4">
-      <!-- <WeeklyStats/> -->
-    </v-col>
-    <!---Daily Activities-->
-    <v-col cols="12" sm="12" lg="4">
-      <DailyActivities/>
-    </v-col>
-  </v-row>
+        <!-- Earned Chart -->
+        <v-col cols="12" sm="3">
+            <Earned />
+        </v-col>
+        <!-- TotalEarning Chart -->
+        <v-col cols="12" sm="3">
+            <TotalEarning />
+        </v-col>
+        <!-- Current Value -->
+        <v-col cols="12">
+            <CurrentValue />
+        </v-col>
+        <v-col cols="12" md="4">
+            <!-- Yearly Breakup -->
+            <YearlyBreakup />
+            <!-- Monthly Earnings -->
+            <MonthlyEarnings />
+            <!-- Most Visited -->
+            <MostVisited />
+        </v-col>
+        <v-col cols="12" md="4">
+            <!-- Yearly Sales -->
+            <YearlySales />
+            <!-- Page Impression -->
+            <PageImpression />
+            <v-row class="mt-3">
+                <v-col cols="12" sm="6">
+                    <Customers />
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <Projects />
+                </v-col>
+            </v-row>
+        </v-col>
+
+        <v-col cols="12" md="4">
+            <v-row>
+                <!-- Revenue Updates -->
+                <v-col cols="12">
+                    <RevenueUpdates />
+                </v-col>
+                <!-- Sales Overview -->
+                <v-col cols="12">
+                    <SalesOverview />
+                </v-col>
+            </v-row>
+        </v-col>
+    </v-row>
 </template>
