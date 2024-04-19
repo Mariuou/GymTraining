f1aaf0c1bb0baee836582a0cd0d0fb5ca6a86050
Jorge <gorgec801@gmail.com>
2024-04-19 00:12:56 -0600

    agregar documentos

---
+++ FrontEnd/src/types/apps/ContactType.ts
@@ -0,0 +1,10 @@
+export type ContactType = {
+    id: string;
+    avatar: string;
+    fecha_a: string;
+    usermail: string;
+    phone: string;
+    estatus: string;
+    resultados: string;
+    resultadosstatus: string;
+};
