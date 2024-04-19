7e680489364e8516806a416f88851a998c6853e7
suri <suripeli04@gmail.com>
2024-04-19 02:28:31 -0600

    cambios de vistas

--- FrontEnd/src/views/dashboards/Training/Dashboard/dashboard.vue
+++ FrontEnd/src/views/dashboards/Training/Dashboard/dashboard.vue
@@ -58,6 +58,7 @@ const breadcrumbs = ref([
             <MonthlyEarnings />
                     <SalesOverview />
         </v-col>
+    
         <v-col cols="12" sm="3">
             <Followers />
         </v-col>
@@ -68,20 +69,20 @@ const breadcrumbs = ref([
             <!-- Most Visited -->
             <!-- <MostVisited /> -->
         </v-col>
-        <v-col cols="12" md="4">
+        <!-- <v-col cols="12" md="4"> -->
             <!-- Yearly Sales -->
             <!-- <YearlySales /> -->
             <!-- Page Impression -->
             <!-- <PageImpression /> -->
-            <v-row class="mt-3">
-                <v-col cols="12" sm="6">
+            <!-- <v-row class="mt-3"> -->
+                <!-- <v-col cols="12" sm="6"> -->
                     <!-- <Customers /> -->
-                </v-col>
-                <v-col cols="12" sm="6">
+                <!-- </v-col> -->
+                <!-- <v-col cols="12" sm="6"> -->
                     <!-- <Projects /> -->
-                </v-col>
-            </v-row>
-        </v-col>
+                <!-- </v-col> -->
+            <!-- </v-row> -->
+        <!-- </v-col> -->
         <v-col cols="12" md="4">
             <v-row>
                 <!-- Revenue Updates -->
